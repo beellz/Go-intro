6658800dca052ca3f8ad599952104cbd758c4b2a
beellz <belal20795@gmail.com>
2020-06-20 22:53:10 +0530

    adding variable

---
+++ variable.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	  var publisher , writer, artist, title string
+	    var year , pageNumber int
+	      var grade float32
+	        title = "Mr. GoToSleep"
+		  writer = "Tracey Hatchet"
+		    artist = "Jewel Tampson"
+		      publisher = " Dizzy Publishing Inc"
+
+		        year = 1997
+			  pageNumber = 14
+			    grade = 6.5
+
+			      fmt.Println(title, "written by", writer, "drawn by", artist , "published by ", publisher , "year of " , year , "read on page Number ", pageNumber , "graded as ", grade )
+
+			      title = "Epic Vol. 1"
+			      // title = "Mr. GoToSleep"
+			        writer = "Ryan N. Shawn"
+				  artist = "Phoebe Paperclips"
+				    publisher = " Dizzy Publishing Inc"
+
+				      year = 2013
+				        pageNumber = 160
+					  grade = 9.0
+
+					    fmt.Println()
+
+
+
+
+					      fmt.Println(title, "written by", writer, "drawn by", artist , "published by ", publisher , "year of " , year , "read on page Number ", pageNumber , "graded as ", grade )
+
+
+				      }
