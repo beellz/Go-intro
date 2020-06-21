e27f89b1c1168187ad2328b9267cd48bfc5c1455
beellz <belal20795@gmail.com>
2020-06-21 15:55:51 +0530

    adding funcitons to the go

---
+++ Fucntion.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+
+func add(x int, y int) int {
+	var out = x + y
+	return out 
+}
+
+func main (){
+
+	num1 := 5
+	num2 := 4
+	result := add(num1, num2)
+	fmt.Println(result)
+}
+
